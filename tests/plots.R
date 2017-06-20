82384e2a7327a0e4119dd29d9cc5cbcd394566f4
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-06-20 14:29:21 -0700

    Spline test moved to separate file

--- tests/plots.R
+++ tests/plots.R
@@ -14,19 +14,3 @@ fts.plot.covariance(X)
 fts.plot.operators(SD, freq=c(-0.2,0,0.45))
 
 fts.plot.filters(XI.est, Ndpc = 3, lags = -2:2, lty = 1, lwd = 2, col= c(2,6,3), one.plot = TRUE)
-
-## TEST spline basis
-basis = create.bspline.basis(c(0,1),nbasis=11)
-X = fts.rar(100,basis = basis)
-
-SD = fts.spectral.density(X,q=20)
-XI.est = fts.dpca.filters(SD)  # finds the optimal filter
-Y.est = fts.dpca.scores(X, dpcs = XI.est)
-Xdpca.est = fts.dpca.KLexpansion(Y.est, XI.est)
-
-#X = fts.rar(100,d=11)
-#fts.plot.operators(fts.cov.structure(X,lags=-2:2), cor = FALSE, grid = TRUE)
-fts.plot.covariance(X)
-fts.plot.operators(SD, freq=c(-0.2,0,0.45))
-
-fts.plot.filters(XI.est, Ndpc = 3, lags = -2:2, lty = 1, lwd = 2, col= c(2,6,3), one.plot = TRUE)
